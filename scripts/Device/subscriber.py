9b2dcbe76d03bc97f4c8bfb318d7340967d645a0
Neal Paras Mehta <nealparas.mehta@corp.knowledgelens.com>
2023-07-07 19:19:57 +0530

    made minor changes

--- scripts/Device/subscriber.py
+++ scripts/Device/subscriber.py
@@ -1,6 +1,6 @@
 from scripts.api.servicers.services import Service
 from scripts.Device.reports.report_generator import GenerateReports
-
+import logging
 class Subscriber:
     def on_connect_a(client, userdata, flags, rc):
         """ 
@@ -8,8 +8,11 @@ class Subscriber:
         params: client, result code, userdata and flags
         output: prints on successful connection and gives the result code
         """
-        print(f"Connected with result code {str(rc)}")
-        client.subscribe("datagen_a")
+        try:
+            print(f"Connected with result code {str(rc)}")
+            client.subscribe("datagen_a")
+        except Exception as e:
+            logging.exception(e)
 
     def on_connect_b(client, userdata, flags, rc):
         """ 
@@ -17,9 +20,11 @@ class Subscriber:
         params: client, result code, userdata and flags
         output: prints on successful connection and gives the result code
         """
-        print(f"Connected with result code {str(rc)}")
-        client.subscribe("datagen_b")
-
+        try:    
+            print(f"Connected with result code {str(rc)}")
+            client.subscribe("datagen_b")
+        except Exception as e:
+            logging.exception(e)
 
 
     def on_message(client,userdata, msg):
@@ -28,10 +33,12 @@ class Subscriber:
         params: client, result code, msg that is published
         output:prints recieved message and the message
         """
-        topic = msg.topic[8]
-        print(f"Received message: {str(msg.payload.decode())}")
-        Service.create_an_item(str(msg.payload.decode()))
-        GenerateReports.create_report(str(msg.payload.decode()),topic)
-
+        try:    
+            topic = msg.topic[8]
+            print(f"Received message: {str(msg.payload.decode())}")
+            Service.create_an_item(str(msg.payload.decode()))
+            GenerateReports.create_report(str(msg.payload.decode()),topic)
+        except Exception as e:
+            logging.exception(e)
 
 
--- scripts/typer/typer_endpoints.py
+++ scripts/typer/typer_endpoints.py
@@ -2,29 +2,61 @@ from scripts.Device.data_generator.Data_Genrator import GenerateData
 import paho.mqtt.client as mqtt
 from scripts.Device.subscriber import Subscriber
 import typer
-
+import logging
 app = typer.Typer()
 
 @app.command()
 def simulate_data_a():
-    GenerateData.simulate_data_a()
+    """ 
+        A function that on being called using the command calls the function simulate_data_a
+        params: NA
+        output: NA
+    """
+    try:
+        GenerateData.simulate_data_a()
+    except Exception as e:
+        logging.exception(e)
 
 @app.command()
 def simulate_data_b():
-    GenerateData.simulate_data_b()
+    """ 
+        A function that on being called using the command calls the function simulate_data_b
+        params: NA
+        output: NA
+    """
+    try:
+        GenerateData.simulate_data_b()
+    except Exception as e:
+        logging.exception(e)
 
 @app.command()
 def connect_and_listen_a():
-    client = mqtt.Client()
-    client.on_connect = Subscriber.on_connect_a
-    client.on_message = Subscriber.on_message
-    client.connect("localhost", 1883, 61)
-    client.loop_forever()
+    """ 
+        A function that on being called using the command subscribes to data for a
+        params: NA
+        output: NA
+    """
+    try:
+        client = mqtt.Client()
+        client.on_connect = Subscriber.on_connect_a
+        client.on_message = Subscriber.on_message
+        client.connect("localhost", 1883, 61)
+        client.loop_forever()
+    except Exception as e:
+        logging.exception(e)
 
 @app.command()
 def connect_and_listen_b():
-    client = mqtt.Client()
-    client.on_connect = Subscriber.on_connect_b
-    client.on_message = Subscriber.on_message
-    client.connect("localhost", 1883, 61)
-    client.loop_forever()
+    """ 
+        A function that on being called using the command subscribes to data for b
+        params: NA
+        output: NA
+    """
+    try:
+        client = mqtt.Client()
+        client.on_connect = Subscriber.on_connect_b
+        client.on_message = Subscriber.on_message
+        client.connect("localhost", 1883, 61)
+        client.loop_forever()
+    except Exception as e:
+        logging.exception(e)
